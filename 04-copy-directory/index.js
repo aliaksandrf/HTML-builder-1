d8de064fd72efe1f39f65cea368a4195909d2861
Aliaksandr Fedukovich <1632637@mail.ru>
2022-11-04 22:03:50 +0300

    feat: add solution for task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,17 +1,47 @@
 const path = require('path');
 const fs = require('fs');
+const { readdir, copyFile, mkdir, rm } = require('fs').promises;
+const { stdout } = process;
 
 
 const source = path.join(__dirname, 'files');
 const purpose = path.join(__dirname, 'files-copy');
 
+(async function() {
+
+  await rm(purpose, {recursive: true, force: true});
+  await mkdir(purpose, {recursive: true});
+  await copyFiles(source, purpose);
 
 
-fs.mkdir(purpose, { recursive: true }, (err) => {
+})();
+
+async function copyFiles (source, purpose) {
+  try {
+    const dir = await readdir(source, { withFileTypes: true });
+    for (const file of dir) {
+      if (file.isFile()) {
+        await copyFile(path.join(source, file.name), path.join(purpose, file.name));
+      } else if (file.isDirectory()) {
+        await mkdir(path.join(to, file.name));
+        await copyDir(path.join(source, file.name), path.join(purpose, file.name));
+      }
+    }
+  } catch (error) {
+    stdout.write(error.message);
+  }
+}
+
+
+
+/* fs.mkdir(purpose, { recursive: true }, (err) => {
   if(err) console.log(err);
-})
+}) */
 
-fs.readdir(source, (err, files) => {
+
+
+
+/* fs.readdir(source, (err, files) => {
   files.forEach((item) => {
     const sourceFile = path.join(source, `${item}`);
     const purposeFile = path.join(purpose, `${item}`);
@@ -19,4 +49,4 @@ fs.readdir(source, (err, files) => {
       if(err) console.log(err);
     })
   })
-})
+}) */
