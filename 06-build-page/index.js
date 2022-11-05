2741b38fe987609fe1a8167b154166c21dd97c77
Aliaksandr Fedukovich <1632637@mail.ru>
2022-11-05 11:58:25 +0300

    feat: add create direction and copy styles file in project-dist for task 6

---
+++ 06-build-page/index.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathDir = path.join(__dirname, 'project-dist');
+
+
+// create project-dist
+
+fs.stat(pathDir, (err) => {
+  if(err) {
+    fs.mkdir(pathDir, (err) => {
+      if(err) console.log(err);
+    });
+  }
+});
+
+
+//copy assets
+
+const pathAssetsSource = path.join(__dirname, 'assets');
+const pathAssetsPurpose = path.join(pathDir, 'assets');
+
+fs.stat(pathAssetsPurpose, (err) => {
+  if(err) {
+    fs.mkdir(pathAssetsPurpose, (err) => {
+      if(err) console.log(err);
+    });
+  }
+});
+
+copyAssets(pathAssetsSource, pathAssetsPurpose);
+
+
+function copyAssets(pathFunction, pathPurpose) {
+  fs.readdir(pathFunction, (err, files) => {
+    if(err) console.log(err);
+
+    files.forEach(item => {
+      const pathItem = path.join(pathFunction, item);
+      const path2 = path.join(pathPurpose, item);
+      fs.stat(pathItem, (err, stats) => {
+        if (err) console.log(err);
+        if(stats.isDirectory()) {
+/*           console.log('isDirectory'); */
+
+          fs.stat(path2, (err) => {
+            if(err) {
+              fs.mkdir(path2, (err) => {
+                if(err) console.log(err);
+              });
+            }
+          });
+
+          copyAssets(pathItem, path2);
+        } else {
+/*           console.log('isFile');
+          console.log(pathItem);
+          console.log(path2); */
+          fs.copyFile(pathItem, path2, err => {
+            if(err) console.log(err);
+          })
+
+        }
+      });
+    })
+  })
+
+}
+
+
+
+
+/* fs.readdir(pathAssetsSource, (err, files) => {
+  if(err) console.log(err);
+  files.forEach(item => {
+    const pathItem = path.join(pathAssetsSource, item);
+    fs.stat(pathItem, (err, stats) => {
+      if (err) console.log(err);
+      if(stats.isDirectory()) {
+        console.log('isDirectory');
+      } else {
+        console.log('isFile');
+      }
+    });
+  })
+}) */
+
+
